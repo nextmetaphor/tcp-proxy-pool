8ae2fe2a63ab1f14a159dfb2dc0bab7d49b0b8a8
nextmetaphor <paul@nextmetaphor.com>
2018-05-28 09:59:21 +0100

    further unit tests

--- controller/pool_test.go
+++ controller/pool_test.go
@@ -12,14 +12,24 @@ type (
 )
 
 var (
-	testContainer = &Container{
+	testContainer1 = &Container{
+		ExternalID: "1",
+		//StartTime:  12,
+	}
+
+	testContainer2 = &Container{
+		ExternalID: "2",
+		//StartTime:  12,
+	}
+
+	testContainer42 = &Container{
 		ExternalID: "42",
 		//StartTime:  12,
 	}
 )
 
 func (tcm TestContainerManager) CreateContainer() *Container {
-	return testContainer
+	return testContainer42
 }
 
 func Test_CreateContainer(t *testing.T) {
@@ -35,6 +45,24 @@ func Test_CreateContainer(t *testing.T) {
 		pool := make(ContainerPool, 0)
 		c, err := CreateContainer(&pool, tcm)
 		assert.Nil(t, err, "nil error should have been returned")
-		assert.Equal(t, testContainer, c, "returned container incorrect")
+		assert.Equal(t, testContainer42, c, "returned container incorrect")
+		assert.Equal(t, 1, len(pool), "pool size incorrect")
+		assert.Equal(t, testContainer42, pool[testContainer42.ExternalID], "incorrect container in pool")
 	})
+
+	t.Run("ExistingPoolNewContainer", func(t *testing.T) {
+		pool := make(ContainerPool, 0)
+		pool["1"] = testContainer1
+		pool["2"] = testContainer2
+
+		c, err := CreateContainer(&pool, tcm)
+
+		assert.Nil(t, err, "nil error should have been returned")
+		assert.Equal(t, testContainer42, c, "returned container incorrect")
+		assert.Equal(t, 3, len(pool), "pool size incorrect")
+		assert.Equal(t, testContainer42, pool[testContainer42.ExternalID], "incorrect container in pool")
+		assert.Equal(t, testContainer1, pool[testContainer1.ExternalID], "incorrect container in pool")
+		assert.Equal(t, testContainer2, pool[testContainer2.ExternalID], "incorrect container in pool")
+	})
+
 }
