4cdf69f4b0a5335f2153d6e8f67084e98bcc327b
nextmetaphor <paul@nextmetaphor.com>
2018-07-19 19:18:43 +0100

    creating interface for monitor

---
+++ monitor/influx.go
@@ -0,0 +1,118 @@
+package monitor
+
+import (
+	"github.com/sirupsen/logrus"
+	"strings"
+	"github.com/influxdata/influxdb/client/v2"
+	"github.com/nextmetaphor/tcp-proxy-pool/log"
+	"time"
+	"net"
+)
+
+// CreateMonitor simply creates a pointer to a Client
+// TODO return error
+func CreateMonitor(ms Settings, l *logrus.Logger) *Client {
+	if strings.TrimSpace(ms.Address) == "" {
+		return nil
+	}
+
+	monitorClient, err := client.NewUDPClient(client.UDPConfig{
+		Addr: ms.Address,
+	})
+	if err != nil {
+		log.Error(logErrorCreatingMonitorConnection, err, l)
+	}
+
+	return &Client{
+		Client:   monitorClient,
+		settings: ms,
+		logger:   l,
+	}
+}
+
+func (mon *Client) writePoint(measurementName string, tags map[string]string, fields map[string]interface{}) {
+	if strings.TrimSpace(mon.settings.Address) == "" {
+		return
+	}
+
+	// TODO - new batch every time???
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  mon.settings.Database,
+		Precision: "ns",
+	})
+	if err != nil {
+		log.Error(logErrorCreatingMonitorBatch, err, mon.logger)
+		return
+	}
+
+	pt, err := client.NewPoint(measurementName, tags, fields, time.Now())
+	if err != nil {
+		log.Error(logErrorCreatingPoint, err, mon.logger)
+		return
+	}
+	bp.AddPoint(pt)
+
+	if mon.Client != nil {
+		if err := (mon.Client).Write(bp); err != nil {
+			log.Error(logErrorWritingPoint, err, mon.logger)
+		}
+	}
+}
+
+// WriteBytesCopied writes the number of bytes copied to the monitor connection
+func (mon *Client) WriteBytesCopied(srcIsServer bool, totalBytesCopied int64, dst, src net.Conn) {
+	var fields map[string]interface{}
+	var tags map[string]string
+	if srcIsServer {
+		fields = map[string]interface{}{fieldCopiedFromServer: totalBytesCopied}
+		tags = map[string]string{
+			tagTCPProxyPoolClientConn: dst.LocalAddr().String(),
+			tagTCPProxyPoolServerConn: src.LocalAddr().String(),
+		}
+	} else {
+		fields = map[string]interface{}{fieldCopiedToServer: totalBytesCopied}
+		tags = map[string]string{
+			tagTCPProxyPoolClientConn: src.LocalAddr().String(),
+			tagTCPProxyPoolServerConn: dst.LocalAddr().String(),
+		}
+	}
+
+	go mon.writePoint(
+		measurementDataTransfer,
+		tags,
+		fields)
+}
+
+// WriteConnectionAccepted writes a point to the monitor to indicate that a connection was accepted
+func (mon *Client) WriteConnectionAccepted(src net.Conn) {
+	go mon.writePoint(
+		measurementConnectionPool,
+		map[string]string{
+			tagTCPProxyPoolClientConn: src.LocalAddr().String(),
+			tagTCPProxyPoolServerConn: src.RemoteAddr().String(),
+		},
+		map[string]interface{}{fieldConnectionsAccepted: 1})
+}
+
+// WriteConnectionRejected writes a point to indicate that a connection was rejected
+func (mon *Client) WriteConnectionRejected(src net.Conn) {
+	go mon.writePoint(
+		measurementConnectionPool,
+		map[string]string{
+			tagTCPProxyPoolClientConn: src.LocalAddr().String(),
+			tagTCPProxyPoolServerConn: src.RemoteAddr().String(),
+		},
+		map[string]interface{}{fieldConnectionsRejected: 1})
+}
+
+// WriteConnectionPoolStats writes a the number of connections in use and the pool size to the monitor
+func (mon *Client) WriteConnectionPoolStats(src net.Conn, connectionsInUse, connectionPoolSize int) {
+	go mon.writePoint(
+		measurementConnectionPool,
+		map[string]string{
+			tagTCPProxyPoolClientConn: src.LocalAddr().String(),
+			tagTCPProxyPoolServerConn: src.RemoteAddr().String()},
+		map[string]interface{}{
+			fieldConnectionsInUse:   connectionsInUse,
+			fieldConnectionPoolSize: connectionPoolSize})
+}
